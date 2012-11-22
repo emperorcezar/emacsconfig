1751067cac695cf3d9bf04b5ae6b4b7411649380
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-11-21 22:24:50 -0600

    Added mk_project

--- init.el
+++ init.el
@@ -216,3 +216,20 @@
     (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
 (ad-activate 'ansi-term)
 (put 'downcase-region 'disabled nil)
+
+;; Projects
+(require 'mk-project)
+(global-set-key (kbd "C-c p c") 'project-compile)
+(global-set-key (kbd "C-c p l") 'project-load)
+(global-set-key (kbd "C-c p a") 'project-ack)
+(global-set-key (kbd "C-c p g") 'project-grep)
+(global-set-key (kbd "C-c p o") 'project-multi-occur)
+(global-set-key (kbd "C-c p u") 'project-unload)
+(global-set-key (kbd "C-c p f") 'project-find-file) ; or project-find-file-ido
+(global-set-key (kbd "C-c p i") 'project-index)
+(global-set-key (kbd "C-c p s") 'project-status)
+(global-set-key (kbd "C-c p h") 'project-home)
+(global-set-key (kbd "C-c p d") 'project-dired)
+(global-set-key (kbd "C-c p t") 'project-tags)
+
+(load "~/.emacs_projects.el")
